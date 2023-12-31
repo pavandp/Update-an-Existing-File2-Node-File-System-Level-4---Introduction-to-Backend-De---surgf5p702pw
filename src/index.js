1805a41ddf8dee337b29debaf31412c8a00f315c
pavankdp <pavankdp29@gmail.com>
2023-12-31 19:48:13 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const fileName = 'myfile.txt';
 const updateFile = async (fileName, fileContent) => {
     //Write your code here to overwrite the file content
     //Don't change function name
-    
+    const existing=await fs.readFile(fileName, 'utf-8');
+    const update=`${existing}${fileContent}`;
+    await fs.writeFile(fileName, update, 'utf-8');
+    return update;
 };
 
 
